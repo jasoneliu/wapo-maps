3833909f1e41819cc6a637c141678b8ec8ab8f4a
Jason Liu <jasoneliu03@gmail.com>
2022-07-25 10:14:21 -0400

    Initial commit

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "ui";
+import { Button as WPDSButton } from "@washingtonpost/wpds-ui-kit";
+
+export default function Web() {
+  return (
+    <div>
+      <h1>Web</h1>
+      <Button />
+      <WPDSButton>Hi, Art</WPDSButton>
+    </div>
+  );
+}
