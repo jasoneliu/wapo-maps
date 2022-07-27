1e8998e9abd9c92696aadcc935f62a71b12f3246
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2022-07-27 16:35:52 -0400

    Feature/geocoding heatmap (#19)
    * reverse-geo-setup
    
    * Fix useReverseGeo hook
    
    * fix axios call
    
    * onClickStyling and latlng for address

--- src/components/maps/maps.tsx
+++ src/components/maps/maps.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { Avatar } from '@mui/material';
 import GoogleMapReact from 'google-map-react';
 import useReverseGeo from 'src/hooks/useReverseGeo';
+import PlaceIcon from '@mui/icons-material/Place';
 
-const Marker = ({ lat, lng, alt, image }) => (
-  <div>
-    <Avatar alt={alt} src={image} />
-  </div>
+const Marker = ({ lat, lng }) => (
+    <div id={`${lat}+${lng}`}>
+      <PlaceIcon fontSize="large" />
+    </div>
 );
 
 export const Map = () => {
@@ -24,43 +24,18 @@ export const Map = () => {
     zoom: 10,
   };
   return (
-    <div style={{ height: '100%', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyAUe57u_rCqLInDSAkBXhjMpCFTil_1NzY' }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-        onClick={(e) => {
-          console.log(e.lat);
-          setLat(e.lat);
-          console.log(e.lng);
-          setLng(e.lng);
-        }}
-      >
-        <Marker
-          lat={38.9028771}
-          lng={-77.0308094}
-          alt="Washington DC"
-          image="https://picsum.photos/100"
-        />
-        <Marker
-          lat={40.6965859}
-          lng={-74.5540074}
-          alt="New York"
-          image="https://picsum.photos/100"
-        />
-        <Marker
-          lat={30.3079827}
-          lng={-97.8934871}
-          alt="Texas"
-          image="https://picsum.photos/100"
-        />
-        <Marker
-          lat={37.3902151}
-          lng={-122.0892005}
-          alt="California"
-          image="https://picsum.photos/100"
-        />
-      </GoogleMapReact>
-    </div>
+      <div style={{ height: '100%', width: '100%' }}>
+        <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyAUe57u_rCqLInDSAkBXhjMpCFTil_1NzY' }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+            onClick={(e) => {
+              setLat(e.lat);
+              setLng(e.lng);
+            }}
+        >
+          <Marker lat={lat} lng={lng} />
+        </GoogleMapReact>
+      </div>
   );
 };
